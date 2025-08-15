87152ef7f2a3098cfd4d9cf464188c8075e2a3eb
Andy Lawler <mrcheeks@gmail.com>
2025-08-15 20:32:50 +1000

    single page

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import { Stack } from 'expo-router';
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function Home() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Rivals 500' }} />
+      <ThemedView style={styles.container}>
+        <ThemedText type="title">Welcome to the Home Screen!</ThemedText>
+      </ThemedView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
